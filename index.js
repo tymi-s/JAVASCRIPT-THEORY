14867e5a71f2e430742c5ffe6db5534f3ecdb5aa
tymi <tymonszyler@gmail.com>
2025-10-27 15:26:22 +0100

    fasdfas

--- index.js
+++ index.js
@@ -87,53 +87,108 @@ commant
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////działąnia na stringach:
 
-let str = "mysia jest fajna";
+// let str = "mysia jest fajna";
 
-console.log( str.charAt(0) ); // returns char at index 0
-console.log( str.indexOf("i") );// index of first occurence of "i"
-console.log( str.lastIndexOf("s") );// index of last occurence of "s"
-console.log( str.replace("mys","pies") );// replaces "mys" with "pies"
-console.log( str.length );// length of string
-console.log( str.trim() );// removes whitespace from both ends
-console.log( str.toUpperCase() )
-console.log( str.repeat(2) )// repeats the string 2 times
-console.log( str.startsWith(" ") )// checks if string starts with "mys"
-console.log ( str.includes("fajna") )// checks if string contains "fajna"
+// console.log( str.charAt(0) ); // returns char at index 0
+// console.log( str.indexOf("i") );// index of first occurence of "i"
+// console.log( str.lastIndexOf("s") );// index of last occurence of "s"
+// console.log( str.replace("mys","pies") );// replaces "mys" with "pies"
+// console.log( str.length );// length of string
+// console.log( str.trim() );// removes whitespace from both ends
+// console.log( str.toUpperCase() )
+// console.log( str.repeat(2) )// repeats the string 2 times
+// console.log( str.startsWith(" ") )// checks if string starts with "mys"
+// console.log ( str.includes("fajna") )// checks if string contains "fajna"
 
-let phoneNumber = "123-456-7890";
-phoneNumber = phoneNumber.replaceAll("-"," ");///////////////////////////
-console.log(phoneNumber);
+// let phoneNumber = "123-456-7890";
+// phoneNumber = phoneNumber.replaceAll("-"," ");///////////////////////////
+// console.log(phoneNumber);
 
-let p = "123"
-p = p.padStart(15,"0") ////////////////
-console.log(p)
+// let p = "123"
+// p = p.padStart(15,"0") ////////////////
+// console.log(p)
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////slicing strings:
 
-const fullname= "Tymon Szyler";
-let firstname= fullname.slice(0,5);
-let lastname= fullname.slice(fullname.indexOf("S"),fullname.length);
+// const fullname= "Tymon Szyler";
+// let firstname= fullname.slice(0,5);
+// let lastname= fullname.slice(fullname.indexOf("S"),fullname.length);
 
-console.log(firstname);
-console.log(lastname);
+// console.log(firstname);
+// console.log(lastname);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////mthod chaining:
 
-let message = "  hello World! Welcome to JavaScript programming.   "; 
-// usuniecie white spaces, przedi i za
-// wszystko do lowercase i pierwszy do uppercase
+// let message = "  hello World! Welcome to JavaScript programming.   "; 
+// // usuniecie white spaces, przedi i za
+// // wszystko do lowercase i pierwszy do uppercase
 
-message= message.trim().charAt(0).toUpperCase() + message.trim().slice(1).toLowerCase();    
+// message= message.trim().charAt(0).toUpperCase() + message.trim().slice(1).toLowerCase();    
 
 
-console.log(message);
+// console.log(message);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////arrays:
 
 
+// let fruits = ["apple","strawbery","banana","orange"];
+
+// console.log(fruits[0]); 
+
+// fruits.push("lichi") // appending
+// console.log(fruits);
+
+// fruits.unshift("mangodb") // adding element to the begining
+// console.log(fruits);
+
+// fruits.shift() //deleting first element in the array
+// console.log("shift: " + fruits);
+
+// fruits.pop("lichi") // poping last element
+// console.log(fruits);
+
+
+// arrayLength = fruits.length;
+// console.log(arrayLength);
+
+
+// let x = fruits.indexOf("banana"); // returns -1 if item not found in an array;
+
+// x!= -1  ? console.log(x): console.log("item not found");
+
+// fruits = fruits.sort()// sorting alfabetically
+// fruits = fruits.reverse(); // reversing array
+
+// //finding minimum and maximum in array using spread operator:
+
+// let arr1=[1,3,6,3,12,768,2,55]
+// let max = Math.max(...arr1);
+// console.log(max);
+
+
+// let min= Math.min(...arr1);
+// console.log(min);
+
+
+// ///spliting string to an array of chars:
+
+// let o = "i hate chockolate people";
+// let m =[...o].join("");
+// m = m.replace("chockolate","black") 
+// console.log(m)
+
+// //combining two arrays into one:
+// let array1= ["a","b","cd"]
+// let array2 = ["e","f","g"]
+// let combined= [...array1, ...array2];
+// console.log(combined)
 
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////map:
+/// map - wykonuje funkcję dla każedego z elementów tablicy i zwraca nową tablicę:
 
-
+tablica = [1,2,3,4]
+nowaTablica= tablica.map(x => x**2)
+console.log(nowaTablica)
